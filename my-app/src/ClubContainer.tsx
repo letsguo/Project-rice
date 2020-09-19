78af62f5c84fe1375770d588e6692e94ee9f26c9
JJ Guo <jjguo123@gmail.com>
2020-09-18 23:06:19 -0700

    Remove boilerplate app.tsx contents and create ClubContainer component

---
+++ my-app/src/ClubContainer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ClubContainer.css'
+
+const ClubContainer = () => {
+    return (
+        <div className={"clubContainer"}>Hello</div>
+    );
+};
+
+export default ClubContainer
