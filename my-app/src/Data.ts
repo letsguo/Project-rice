dae2138a729f90a41750878d21363a7732512d05
Sabreena Yang <sabreenayang8@gmail.com>
2020-09-19 22:00:07 -0700

    Added in more clubs

--- my-app/src/Data.ts
+++ my-app/src/Data.ts
@@ -2,7 +2,8 @@ export interface ClubResponse {
     "name": string
     "school": string
     "location": string
-    "tags": any[]
+    "openToCollaborate"?: boolean
+    "applicationNeeded"?: boolean
     "imagePath": string
 }
 
@@ -10,37 +11,87 @@ export const clubOne: ClubResponse = {
     name: "Investment Club",
     school: "Camas High School",
     location: "Camas, WA",
-    tags: [
-        {
-            "Application Needed": false
-        },
-        {
-            "Open to collab": true
-        }
-    ],
-    imagePath: "./img/true.png"
+    imagePath: "./img/true.png",
+    openToCollaborate: false
 };
 
 export const clubTwo: ClubResponse = {
     name: "Robotics Club",
     school: "Union High School",
     location: "Camas, WA",
-    tags: [
-        {
-            "Application Needed": true
-        }
-    ],
-    imagePath: "./img/true.png"
+    imagePath: "./img/true.png",
+    openToCollaborate: true
 };
 
 export const clubThree: ClubResponse = {
     name: "Key Club",
     school: "Camas High School",
     location: "Camas, WA",
-    tags: [
-        {
-            "Application Needed": true
-        }
-    ],
-    imagePath: "./img/true.png"
+    imagePath: "./img/true.png",
+    openToCollaborate: true
 };
+
+export const clubFour: ClubResponse = {
+    name: "DECA",
+    school: "South Salem High School",
+    location: "Salem, OR",
+    imagePath: "./img/true.png",
+    openToCollaborate: true
+};
+
+export const clubFive: ClubResponse = {
+    name: "DECA",
+    school: "North Salem High School",
+    location: "Salem, MA",
+    imagePath: "./img/true.png",
+    openToCollaborate: true
+};
+
+export const clubSix: ClubResponse = {
+    name: "Symphony Orchestra",
+    school: "California High School",
+    location: "San Ramon, CA",
+    imagePath: "./img/true.png",
+    openToCollaborate: true
+};
+
+export const clubSeven: ClubResponse = {
+    name: "Health Occupations Students of America",
+    school: "West Salem High School",
+    location: "West Salem, WI",
+    imagePath: "./img/true.png",
+    openToCollaborate: true
+};
+
+export const clubEight: ClubResponse = {
+    name: "Women in Computer Science",
+    school: "Torrence High School",
+    location: "San Diego, CA",
+    imagePath: "./img/true.png",
+    openToCollaborate: true
+};
+
+export const clubNine: ClubResponse = {
+    name: "Investment Club",
+    school: "Norwell High School",
+    location: "Norwell, MA",
+    imagePath: "./img/true.png",
+    openToCollaborate: true
+};
+
+export const clubTen: ClubResponse = {
+    name: "Speech & Debate",
+    school: "Blanchet Catholic School",
+    location: "Salem,OR",
+    imagePath: "./img/true.png",
+    openToCollaborate: true
+};
+
+export const clubEleven: ClubResponse = {
+    name: "Debate Club",
+    school: "Salem Academy Christian Schools",
+    location: "Salem,OR",
+    imagePath: "./img/true.png",
+    openToCollaborate: true
+};
+
--- my-app/src/components/ClubPage.tsx
+++ my-app/src/components/ClubPage.tsx
@@ -3,7 +3,7 @@ import './ClubContainer.css'
 import ClubContainer from "./ClubContainer";
 import {useRecoilState} from "recoil";
 import {openToCollabState, searchState} from "../recoil/atom";
-import {clubOne, clubTwo, clubThree, ClubResponse} from "../Data";
+import {clubOne, clubTwo, clubThree, clubFour, clubFive, clubSix, clubSeven, clubEight, clubNine, clubTen, clubEleven, ClubResponse} from "../Data";
 
 
 interface ClubPageProps {
@@ -13,14 +13,14 @@ interface ClubPageProps {
 const ClubPage: React.FC<ClubPageProps> = ({
      title
 }) => {
-    const clubResponses: ClubResponse[] = [clubOne, clubTwo, clubThree];
+    const clubResponses: ClubResponse[] = [clubOne, clubTwo, clubThree, clubFour, clubFive, clubSix, clubSeven, clubEight, clubNine, clubTen, clubEleven];
 
     const [searchQuery] = useRecoilState(searchState);
     const [isOpenToCollab] = useRecoilState(openToCollabState);
 
     const showClubs = clubResponses.filter(item => {
         return item.name.toLowerCase().includes(searchQuery.toLowerCase()) &&
-            isOpenToCollab && item.tags[0]["Application Needed"]
+            isOpenToCollab && item.openToCollaborate
     });
 
     const loadClubParts = (messages: ClubResponse[], startIdx: number) => (
