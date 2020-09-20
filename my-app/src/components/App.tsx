2d5f97270a6263414a398dd68c8d1a50343bb5f4
JJ Guo <jjguo123@gmail.com>
2020-09-19 22:29:59 -0700

    Put in routing, to enable the ClubDetails page

--- my-app/src/components/App.tsx
+++ my-app/src/components/App.tsx
@@ -4,13 +4,29 @@ import ClubPage from "./ClubPage";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FilteringContainer from "./FilteringContainer";
 import {RecoilRoot} from "recoil";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import ClubDetails from "./ClubDetails";
 
 function App() {
   return (
     <div className="App row">
         <RecoilRoot>
-            <FilteringContainer />
-            <ClubPage title={"Welcome to Clubby"}/>
+            <Router>
+                <Switch>
+                    <Route path={"/club"}>
+                        <ClubDetails/>
+                    </Route>
+                    <Route path={"/"}>
+                        <FilteringContainer/>
+                        <ClubPage title={"title"}/>
+                    </Route>
+                </Switch>
+            </Router>
         </RecoilRoot>
     </div>
   );
